dcfef38aa919387eb34ba66afc731a285aec7e11
iSara-8 <112809770+iSara-8@users.noreply.github.com>
2023-05-06 14:35:02 +0300

    commit 2

--- index2.php
+++ index2.php
@@ -12,15 +12,15 @@ if (!isset($_SESSION["user"])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
-    <title>User Dashboard</title>
+    <title>الصفحة الرئيسية</title>
 </head>
 <body>
     <div class="container">
-        <h1>Welcome to Dashboard</h1>
+        <h1>أهلًا بك في صفحتك</h1>
         <a href="logout.php" class="btn btn-warning">Logout</a>
     </div>
 
-    <h3>Temperature monitor</h3>
+    <h3>مراقبة درجة الحرارة</h3>
     <iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/2135382/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&yaxis=temperature"></iframe>
 </body>
 </html>
--- login.php
+++ login.php
@@ -11,7 +11,7 @@ if (isset($_SESSION["user"])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>تسجيل جديد</title>
+    <title>تسجيل الدخول</title>
 
      <!-- font awesome cdn link  -->
 
