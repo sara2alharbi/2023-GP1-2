6a7a169e142f8e53fc226b62a4e530ee3e912e68
iSara-8 <112809770+iSara-8@users.noreply.github.com>
2023-10-21 16:47:33 +0300

    Sprint 4_commit#10

--- api.php
+++ api.php
@@ -12,21 +12,38 @@ function getRoomState($id){
     global $con;
     $sql = "SELECT noise FROM `noise` WHERE microID = '$id' ORDER BY Date_today DESC, Time_today DESC LIMIT 1";
     $query = mysqli_query($con, $sql);
-    $noise = mysqli_fetch_assoc($query)['noise'];
+    $row = mysqli_fetch_assoc($query);
+
+    $data = [
+        'noise' => (empty($row['noise']) || !mb_check_encoding($row['noise'], 'UTF-8')) ? 'لا يوجد بيانات' : $row['noise'],
+    ];
+
     $sql = "SELECT temperature, humidity FROM `temperature` WHERE microID = '$id' ORDER BY Date_today DESC, Time_today DESC LIMIT 1";
     $query = mysqli_query($con, $sql);
-    $res = mysqli_fetch_assoc($query);
+    $row = mysqli_fetch_assoc($query);
+
+    $data['temp'] = (empty($row['temperature']) || !mb_check_encoding($row['temperature'], 'UTF-8')) ? 'لا يوجد بيانات' : $row['temperature'];
+    $data['hum'] = (empty($row['humidity']) || !mb_check_encoding($row['humidity'], 'UTF-8')) ? 'لا يوجد بيانات' : $row['humidity'];
+
     $sql = "SELECT airquality FROM `airquality` WHERE microID = '$id' ORDER BY Date_today DESC, Time_today DESC LIMIT 1";
     $query = mysqli_query($con, $sql);
-    $air = mysqli_fetch_array($query)[0];
-    $temp = $res['temperature'];
-    $humidity = $res['humidity'];
-    return $data = [
-        'noise' => $noise,
-        'temp' => $temp,
-        'hum' => $humidity,
-        'air' => $air
-    ];
+    $row = mysqli_fetch_array($query);
+
+    if ($row !== null && isset($row[0])) {
+        if ($row[0] === null) {
+            $data['air'] = 'لا يوجد بيانات';
+        } elseif ($row[0] == 0) {
+            $data['air'] = 'منخفضة';
+        } elseif ($row[0] == 1) {
+            $data['air'] = 'جيدة';
+        } else {
+            $data['air'] = 'قيمة غير معروفة'; // Handle any other unexpected values
+        }
+    } else {
+        $data['air'] = 'لا يوجد بيانات';
+    }
+
+    return $data;
 }
 
 
