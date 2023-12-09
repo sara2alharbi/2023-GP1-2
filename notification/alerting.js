8c5047079818dfc09f01c0833f1e659dfe8bc8ea
iSara-8 <112809770+iSara-8@users.noreply.github.com>
2023-12-09 15:07:24 +0300

    alerts_updates

--- notification/alerting.js
+++ notification/alerting.js
@@ -1,7 +1,4 @@
-// Function to check for new sensor data and display alerts
-var newNotificationsCount = 0;
-var displayedNotificationIdsTemps = [];
-var displayedNotificationIdsAir = [];
+
 var storedAlertTimes = []; // Array to track stored alert times
 
 
@@ -15,26 +12,6 @@ function checkForAlerts() {
                 return;
             } else {
                 items.forEach(function (data) {
-
-                    if (data.type === "temperature") {
-                        if (displayedNotificationIdsTemps.includes(data.temperature_id)) {
-                            return; // Skip if already displayed
-                        }
-                        displayedNotificationIdsTemps.push(data.temperature_id);
-                    } else {
-                        if (displayedNotificationIdsAir.includes(data.air_id)) {
-                            return; // Skip if already displayed
-                        }
-                        displayedNotificationIdsAir.push(data.air_id);
-                    }
-                    // Increment the notification count
-                    newNotificationsCount++;
-
-
-                    // Update the notification count
-                    $('#notification-count').text(newNotificationsCount);
-                    $('#notification-count').show();
-
                     // Append the new notification to the dropdown list
                     var alertHtml = '';
                     var notificationMessage = '';
@@ -42,7 +19,7 @@ function checkForAlerts() {
                     const modifiedTime = removeSecondsFromTime(data.time);
 
                     if (data.type === 'combined') {                    
-                        notificationMessage = '<h6>درجة الحرارة مرتفعة</h6>' +
+                        notificationMessage = '<h6>جودة الهواء منخفضة و درجة الحرارة مرتفعة</h6>' +
                             '<p>الوقت ' + modifiedTime + '</p>' +
                             '<p> في الغرفة رقم ' + data.room + '</p>' +
                             '<p> درجة الحرارة ' + data.temperature + ' °C</p>';
@@ -74,8 +51,7 @@ function checkForAlerts() {
 
                     // Pass the string as the message to the Notify function
                     Notify(notificationMessage, null, null, 'danger');
-                    // Append the new row to the table
-                    //$('#alerts-table').append(alertHtml);
+
                 });
             }
         }
@@ -131,61 +107,6 @@ function removeSecondsFromTime(timeString) {
     return newTime;
 }
 
-
-// Function to remove a notification from the dropdown list
-function removeNotification(button) {
-    var type = (typeof $(button).data('type') !== 'undefined') ? $(button).data('type') : 'defaultType';
-
-    var temperatureId = $(button).data('id');
-    var airId = $(button).data('air');
-    if (temperatureId === 'undefined') {
-        temperatureId = 0;
-    }
-
-    if (airId === 'undefined') {
-        airId = 0;
-    }
-
-    // Now you can use the 'temperatureId' and 'airId' values in your function
-    console.log("Temperature ID1: " + temperatureId);
-    console.log("Air ID: " + airId);
-    console.log("Notification Type : " + type);
-
-    $.ajax({
-        url: 'delete_notification.php', // Replace with the URL of your PHP script
-        method: 'POST', // You can use POST or GET depending on your needs
-        data: {
-            temperature_id: temperatureId,
-            air_id: airId,
-            type: type,
-        },
-        success: function (response) {
-            console.log('Data added to the database:', response);
-        },
-        error: function (xhr, status, error) {
-            console.error('Error adding data to the database:', error);
-        }
-    });
-
-    $(button).parent().remove();
-
-    // Decrease the notification count
-    newNotificationsCount = parseInt($('#notification-count').text()) || 0;
-    newNotificationsCount--;
-
-    if (newNotificationsCount === 0) {
-        $('#notification-count').hide();
-        $('#alerts-container').hide();
-    } else {
-        $('#notification-count').text(newNotificationsCount);
-    }
-}
-
-// Function to toggle the visibility of the alerts dropdown
-function toggleAlerts() {
-    $('#alerts-dropdown').toggle();
-    $('#alerts-container').show();
-}
 
 // Initialize by checking for alerts immediately
 checkForAlerts();
