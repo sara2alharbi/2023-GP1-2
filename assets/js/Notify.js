26196da192039591d5c22b0bc5e58d1836660a21
iSara-8 <112809770+iSara-8@users.noreply.github.com>
2023-12-11 14:15:25 +0300

    alerts_4

--- assets/js/Notify.js
+++ assets/js/Notify.js
@@ -1,24 +1,22 @@
-Notify = function (text, callback, close_callback, style) {
+Notify = function(text, callback, close_callback, style) {
 
     var time = '15000';
     var $container = $('#notifications');
     var icon = '<i class="fa fa-info-circle "></i>';
 
-    if (typeof style == 'undefined') style = 'warning';
+    if (typeof style == 'undefined' ) style = 'warning'
 
-    var html = $('<div class="alert alert-' + style + '  hide">' + icon + " " + text + '</div>');
+    var html = $('<div class="alert alert-' + style + '  hide">' + icon +  " " + text + '</div>');
 
-    $('<a>', {
+    $('<a>',{
         text: '×',
         class: 'button close',
         style: 'padding-left: 10px;',
         href: '#',
-        click: function (e) {
+        click: function(e){
             e.preventDefault()
             close_callback && close_callback()
-            remove_notice();
-            // Save the dismissed alert in local storage
-            saveDismissedAlert(text);
+            remove_notice()
         }
     }).prependTo(html)
 
@@ -29,21 +27,21 @@ Notify = function (text, callback, close_callback, style) {
         html.stop().fadeOut('slow').remove()
     }
 
-    var timer = setInterval(remove_notice, time);
+    var timer =  setInterval(remove_notice, time);
 
-    $(html).hover(function () {
+    $(html).hover(function(){
         clearInterval(timer);
-    }, function () {
+    }, function(){
         timer = setInterval(remove_notice, time);
     });
 
     html.on('click', function () {
         clearInterval(timer)
         callback && callback()
-        remove_notice();
-        // Save the dismissed alert in local storage
-        saveDismissedAlert(text);
+        remove_notice()
     });
+
+
 }
 
 // Function to clear notifications when navigating to a new page
@@ -55,24 +53,3 @@ function clearNotifications() {
 $(window).on('beforeunload', function () {
     clearNotifications();
 });
-
-// Function to save dismissed alerts in local storage
-function saveDismissedAlert(alertText) {
-    // Get the dismissed alerts array from local storage
-    var dismissedAlerts = JSON.parse(localStorage.getItem('dismissedAlerts')) || [];
-
-    // Add the current alert text to the dismissed alerts array
-    dismissedAlerts.push(alertText);
-
-    // Save the dismissed alerts array back to local storage
-    localStorage.setItem('dismissedAlerts', JSON.stringify(dismissedAlerts));
-}
-
-// Function to check if an alert has been dismissed
-function isAlertDismissed(alertText) {
-    // Get the dismissed alerts array from local storage
-    var dismissedAlerts = JSON.parse(localStorage.getItem('dismissedAlerts')) || [];
-
-    // Check if the current alert text is in the dismissed alerts array
-    return dismissedAlerts.includes(alertText);
-}
--- notification/alerting.js
+++ notification/alerting.js
@@ -1,6 +1,31 @@
 
 var storedAlertTimes = []; // Array to track stored alert times
 
+// Function to generate a unique key for the alert
+function getAlertKey(data) {
+    return `${data.type}_${data.date}_${data.time}_${data.room}`;
+}
+
+// Function to save dismissed alerts in session storage
+function saveDismissedAlert(alertKey) {
+    // Get the dismissed alerts array from session storage
+    var dismissedAlerts = JSON.parse(sessionStorage.getItem('dismissedAlerts')) || [];
+
+    // Add the current alert key to the dismissed alerts array
+    dismissedAlerts.push(alertKey);
+
+    // Save the dismissed alerts array back to session storage
+    sessionStorage.setItem('dismissedAlerts', JSON.stringify(dismissedAlerts));
+}
+
+// Function to check if an alert has been dismissed
+function isAlertDismissed(alertKey) {
+    // Get the dismissed alerts array from session storage
+    var dismissedAlerts = JSON.parse(sessionStorage.getItem('dismissedAlerts')) || [];
+
+    // Check if the current alert key is in the dismissed alerts array
+    return dismissedAlerts.includes(alertKey);
+}
 
 function checkForAlerts() {
     $.ajax({
@@ -12,9 +37,10 @@ function checkForAlerts() {
                 return;
             } else {
                 items.forEach(function (data) {
-                    var notificationMessage = '';
-                    console.log('General air Id' + data.air_id);
-                    const modifiedTime = removeSecondsFromTime(data.time);
+                    const alertKey = getAlertKey(data);
+                    if (!isAlertDismissed(alertKey)) {
+                        var notificationMessage = '';
+                        const modifiedTime = removeSecondsFromTime(data.time);
 
                     if (data.type === 'combined') {                    
                         notificationMessage = '<h6>جودة الهواء منخفضة و درجة الحرارة مرتفعة</h6>' +
@@ -47,8 +73,9 @@ function checkForAlerts() {
                         notification: message
                     });
 
-                    // Pass the string as the message to the Notify function
                     Notify(notificationMessage, null, null, 'danger');
+                        saveDismissedAlert(alertKey);
+                }
                 });
             }
         }
@@ -104,6 +131,16 @@ function removeSecondsFromTime(timeString) {
     return newTime;
 }
 
+
+// Function to clear dismissed alerts when navigating to a new page
+function clearDismissedAlerts() {
+    sessionStorage.removeItem('dismissedAlerts');
+}
+
+// Clear dismissed alerts when navigating to a new page
+$(window).on('beforeunload', function () {
+    clearDismissedAlerts();
+});
 
 // Initialize by checking for alerts immediately
 checkForAlerts();
